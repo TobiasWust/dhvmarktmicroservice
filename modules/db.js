f18432b81c220a971ec97d15323d7acc5b6a1f75
Tobias Wust <kontakt@tobiaswust.de>
2019-12-13 19:34:28 +0100

    move stuff to /modules

--- modules/db.js
+++ modules/db.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-const config = require('./config');
+const config = require('../config');
 
 mongoose.connect(config.murl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }, error => {
-    console.log(error ? error : 'connected to db');
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, error => {
+  console.log(error ? error : 'connected to db');
 });
 
 const offerSchema = new mongoose.Schema({
@@ -15,18 +15,24 @@ const offerSchema = new mongoose.Schema({
   price: Number,
 });
 
+const searchAgentSchema = new mongoose.Schema({
+  email: String,
+  expirationDate: Number,
+  lastCheck: String,
+});
+
 const Offer = mongoose.model('Offer', offerSchema);
 
 const db = {
   save(offer) {
-    (new Offer(sanitize(offer))).save(err => {if (err) console.error(err)});
+    (new Offer(sanitize(offer))).save(err => { if (err) console.error(err) });
   },
 
   sanitize(offer) {
     r.link = offer.link
     r.date = offer.date
     r.title = offer.title
-    r.price  = offer.price
+    r.price = offer.price
     return r;
   },
 
